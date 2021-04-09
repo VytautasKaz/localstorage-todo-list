483cc2d2355721b9d1ffce3e78e1d043207b6fba
VytautasKaz <vytautas.kazakevicius2@gmail.com>
2021-04-09 20:18:29 +0300

    app functionality update

--- js/main.js
+++ js/main.js
@@ -1,11 +1,43 @@
+let todosList = document.querySelector('ul');
+let formField = document.querySelector('form');
+let userInput = document.querySelector('#user_input');
+let clearButton = document.querySelector('#clear-all');
+
+// Creates a new todo element, sets its value and inserts into the todo list
+
+function todoMaker(input) {
+    let newTodo = document.createElement('li');
+    newTodo.textContent = input;
+    todosList.appendChild(newTodo);
+    userInput.value = '';
+}
+
 // Add a todo
+
+let todosArray = localStorage.getItem('todos')
+    ? JSON.parse(localStorage.getItem('todos'))
+    : [];
+
+localStorage.setItem('todos', JSON.stringify(todosArray));
+
+let storage = JSON.parse(localStorage.getItem('todos'));
+
+formField.addEventListener('submit', function (e) {
+    e.preventDefault();
+    todosArray.push(userInput.value);
+    localStorage.setItem('todos', JSON.stringify(todosArray));
+    todoMaker(userInput.value);
+});
+
+// Display localstorage content when page
+
+for (let i = 0; i < storage.length; i++) {
+    todoMaker(storage[i]);
+}
 
 // Erase all todos
 
-document.querySelector('#clear-all').addEventListener('click', function () {
-    let todosList = document.querySelector('ul');
-
-    while (todosList.firstChild) {
-        todosList.removeChild(todosList.firstChild);
-    }
+clearButton.addEventListener('click', function () {
+    localStorage.clear();
+    location.reload();
 });
